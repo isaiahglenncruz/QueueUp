9b08c01bbaa9592ceac3cb148ad28565d3da4229
Isaiah Cruz <72627685+isaiahglenncruz@users.noreply.github.com>
2021-06-06 20:07:51 -0700

    messages database included

--- models.py
+++ models.py
@@ -36,27 +36,27 @@ def get_user():
 db.define_table(
     'profiles',
     Field('user', 'reference auth_user'),
-    Field('region', default="NA West"), # maybe don't want this as default
+    Field('region', default="NA West"),
     Field('bio'),
-    Field('mic'),
+    Field('mic', 'boolean', default=False),
     Field('tiltproof', 'integer', default=0),
-    Field('leader', 'integer', default=0),
+    Field('leader', 'integer', default=0 ),
     Field('fun', 'integer', default=0),
-    Field('communicative', 'integer', default=0),
+    Field('communicative', 'integer', default=0)
 )
 
 db.define_table(
     'game_data',
     Field('profile', 'reference profiles'),
     Field('game'),
-    Field('gamertag'),
-    Field('rank'),
+    Field('gamertag', default="No Name"),
+    Field('rank', default="Unranked"),
 )
 
 db.define_table(
     'lobbies',
     Field('game'),
-    Field('leader'),
+    Field('leader'), # change to a reference
     Field('bio'),
     Field('player1'), #
     Field('player2'), # Might need to change the way we do these bc its not portable
@@ -65,7 +65,6 @@ db.define_table(
     Field('rank'),
     Field('region'),
     Field('playstyle'),
-    Field('microphone'),
 )
 
 db.define_table(
@@ -75,6 +74,5 @@ db.define_table(
     Field('name'),
     Field('message'),
 )
-
 
 db.commit()
